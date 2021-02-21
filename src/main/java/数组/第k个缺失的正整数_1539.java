6544efcf6560ce798e53c96367dd5dbb4f8d90a7
贾唐浩 <476736586@qq.com>
2021-02-21 13:30:51 +0800

    数组：第k个缺失的正整数_1539

---
+++ src/main/java/数组/第k个缺失的正整数_1539.java
@@ -0,0 +1,21 @@
+package 数组;
+
+public class 第k个缺失的正整数_1539 {
+    public int findKthPositive(int[] arr, int k) {
+        if (arr[0] > k) {
+            return k;
+        }
+
+        int l = 0, r = arr.length;
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            int x = mid < arr.length ? arr[mid] : Integer.MAX_VALUE;
+            if (x - mid - 1 >= k) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return k + l;
+    }
+}
---
+++ src/test/java/数组/第k个缺失的正整数_1539Test.java
@@ -0,0 +1,18 @@
+package 数组;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class 第k个缺失的正整数_1539Test {
+
+    @Test
+    void findKthPositive() {
+        int[] arr1 = {2, 3, 4, 7, 11};
+        int[] arr2 = {1, 2, 3, 4};
+        第k个缺失的正整数_1539 obj = new 第k个缺失的正整数_1539();
+        Assertions.assertEquals(9, obj.findKthPositive(arr1, 5));
+        Assertions.assertEquals(6, obj.findKthPositive(arr2, 2));
+    }
+}

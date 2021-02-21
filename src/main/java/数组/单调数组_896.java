0dc52597d5827ed0d19d8e9bf50de22b9f25dbb6
贾唐浩 <476736586@qq.com>
2021-02-21 09:45:31 +0800

    数组：单调数组_896 第二版

--- src/main/java/数组/单调数组_896.java
+++ src/main/java/数组/单调数组_896.java
@@ -2,30 +2,22 @@ package 数组;
 
 public class 单调数组_896 {
     public boolean isMonotonic(int[] A) {
-        if (A.length == 1) {
-            return true;
-        }
-        int i = 0;
-        while (i < A.length - 1) {
-            if (A[i] != A[i + 1]) {
-                break;
+        return isDecrease(A) || isIncrease(A);
+    }
+
+    private boolean isIncrease(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i+1]) {
+                return false;
             }
-            i++;
         }
-        if (i == A.length - 1) {
-            return true;
-        }
-        if (A[i] < A[i + 1]) {
-            for (int j = i + 1; j < A.length - 1; j++) {
-                if (A[j] > A[j + 1]) {
-                    return false;
-                }
-            }
-        } else {
-            for (int j = i + 1; j < A.length - 1; j++) {
-                if (A[j] < A[j + 1]) {
-                    return false;
-                }
+        return true;
+    }
+
+    private boolean isDecrease(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] < a[i+1]) {
+                return false;
             }
         }
         return true;

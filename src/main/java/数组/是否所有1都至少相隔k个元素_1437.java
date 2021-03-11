503932b0019b068e09beba9c2a3301af9525bd3a
贾唐浩 <476736586@qq.com>
2021-03-11 23:25:02 +0800

    数组：是否所有1都至少相隔k个元素_1437，解法2

--- src/main/java/数组/是否所有1都至少相隔k个元素_1437.java
+++ src/main/java/数组/是否所有1都至少相隔k个元素_1437.java
@@ -1,22 +1,14 @@
 package 数组;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class 是否所有1都至少相隔k个元素_1437 {
     public boolean kLengthApart(int[] nums, int k) {
-        List<Integer> locations = new ArrayList<>();
+        int prev = -1;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
-                locations.add(i);
-            }
-        }
-        if (locations.size() <= 1) {
-            return false;
-        }
-        for (int i = 0; i < locations.size() - 1; i++) {
-            if (locations.get(i + 1) - locations.get(i) < k + 1) {
-                return false;
+                if (prev != -1 && i - prev - 1 < k) {
+                    return false;
+                }
+                prev = i;
             }
         }
         return true;
